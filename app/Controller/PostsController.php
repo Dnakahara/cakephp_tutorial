6de605de29bbc55aa2b78832f4dd566bd9442190
Dnakahara <d_nakahara@funteam.co.jp>
2016-10-27 11:14:27 +0900

    plugins

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -24,6 +24,7 @@ class PostsController extends AppController{
 		parent::beforeFilter();
 
 		if(!$this->isAuthorized($this->Auth->user())){
+			$this->Flash('');
 			$this->redirect(array(
 				'controller'=>'posts',
 				'action'=>'index',
@@ -53,7 +54,7 @@ class PostsController extends AppController{
 	public function add(){
 		if($this->request->is('post')){
 			$this->Post->create();
-			$this->request->data['Post']['user_id'] = $user_id;
+			$this->request->data['Post']['user_id'] = $this->Auth->user('id');
 			if($this->Post->save($this->request->data)){
 				$this->Flash->success(__('Your post has been saved.'));
 				$this->redirect(array('action'=>'index'));
